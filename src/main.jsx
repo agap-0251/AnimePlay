d695251ed60905459fd8061ee51e4ad048a9f788
Anudeep Dugyala <dugyalaanudeeprao1005@gmail.com>
2023-10-01 20:54:04 +0530

    analytics

--- src/main.jsx
+++ src/main.jsx
@@ -7,12 +7,14 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'
+import {Analytics} from "@vercel/analytics/react"
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
     <BrowserRouter>
       <App />
+      <Analytics />
       <ToastContainer />
     </BrowserRouter>  
   //  </React.StrictMode> 
